a7af1350f4d34cdba24548b4b8c56b51f4da4513
uchoavaz <joaouchoavaz@hotmail.com>
2016-03-21 14:44:48 -0300

    Adicionado admin de usuario

--- projeto_fono/accounts/admin.py
+++ projeto_fono/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import FonoUser
 
-# Register your models here.
+class FonoUserAdmin(admin.ModelAdmin):
+    list_filter = ['enrollment']
+    list_display = (
+        'enrollment', 'username', 'email', 'is_staff',
+        'is_active', 'is_superuser')
+admin.site.register(FonoUser, FonoUserAdmin)
